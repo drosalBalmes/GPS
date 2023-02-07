c0b393747c290aa5297e117c9231c6630eab55ff
david <drosal@jaumebalmes.net>
2023-02-07 20:09:52 +0100

    marca no canvia valors

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -18,56 +19,24 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvLat, tvLng;
-    Button coords;
 
-    private FusedLocationProviderClient fusedLocationClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvLat = (TextView) findViewById(R.id.lat);
-        tvLng = (TextView) findViewById(R.id.lng);
-        coords = findViewById(R.id.getCoords);
-
-        coords.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                getCoords();
-            }
-        });
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
     }
 
-    private void getCoords() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(
-                        new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                        1);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        2);
-            }
-            return;
-        }
-        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // Got last known location. In some rare situations this can be null.
-                if(location != null){
+    public void toCoords(View view){
+        Intent intent = new Intent(this,Coords.class);
+        startActivity(intent);
+    }
 
-                    Toast.makeText(getApplicationContext(),"Latitude: " + location.getLatitude() + ", Longitude: " + location.getLongitude(), Toast.LENGTH_LONG).show();
-                    tvLat.setText(String.valueOf( location.getLatitude()));
-                    tvLng.setText(String.valueOf( location.getLongitude()));
-                }
-            }
-        });
+    public void toMaps(View view){
+        Intent intent = new Intent(this,Maps.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/gps/Maps.java
+++ app/src/main/java/com/example/gps/Maps.java
@@ -1,10 +1,16 @@
 package com.example.gps;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -15,6 +21,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.gps.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,17 +30,21 @@ import java.util.Locale;
 public class Maps extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private FusedLocationProviderClient location;
-    LatLng marca = new LatLng(2,2);
+    private FusedLocationProviderClient fusedLocationClient;
+
+    LatLng marca;
     private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        location = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        marca = new LatLng(4,4);
+        getCoords();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -50,14 +61,19 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //getCoords();
+
+        //Toast.makeText(getApplicationContext(), "Latitude: " + marca.latitude + ", Longitude: " + marca.longitude, Toast.LENGTH_LONG).show();
+
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in the actual pos and move the camera
+
+        mMap.addMarker(new MarkerOptions().position(marca).title((String) getTitle()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marca));
     }
 
     public StringBuilder getTitle(LatLng pob) throws IOException {
@@ -69,10 +85,50 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
             StringBuilder poblacio = new StringBuilder(String.valueOf(addressList.get(0).getLocality()));
             String adress = String.valueOf(addressList.get(0).getAddressLine(0));
 
-            poblacio.append(adress);
+            poblacio.append( " " + adress);
             return poblacio;
         }
         return new StringBuilder("xd");
 
     }
+
+    public void getCoords() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        2);
+            }
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+
+                    marca = new LatLng(location.getLatitude(),location.getLongitude());
+
+                    Toast.makeText(getApplicationContext(), "Latitude: " + marca.latitude + ", Longitude: " + marca.longitude, Toast.LENGTH_LONG).show();
+
+                    //Toast.makeText(getApplicationContext(), "Latitude: " + location.getLatitude() + ", Longitude: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+                    //lat = location.getLatitude();
+                    //lng = location.getLongitude();
+
+                }
+            }
+        });
+        //Toast.makeText(getApplicationContext(), "Latitude: " + marca.latitude + ", Longitude: " + marca.longitude, Toast.LENGTH_LONG).show();
+
+    }
+
+
+
+
 }
+
